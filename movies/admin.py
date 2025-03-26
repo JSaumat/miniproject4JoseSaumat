668f43c2cc14d4c6e873365f58687fdc4a1ef2e3
JSaumat <j_saumat@mail.fhsu.edu>
2025-03-26 15:34:24 -0700

    Created .env and .gitignore.

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Movie
+
 # Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'vote_count')
+    search_fields = ('title',)
