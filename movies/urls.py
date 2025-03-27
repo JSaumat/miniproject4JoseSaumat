a51b3dac72d55c2829dedb6937ff6140cf85bae6
JSaumat <j_saumat@mail.fhsu.edu>
2025-03-26 17:47:19 -0700

    Added a search feature that pulls from the TMDB so that any movie can be added to the voting page. Added voting buttons to the main page that keep track of how many people voted for the movie.

--- movies/urls.py
+++ movies/urls.py
@@ -6,12 +6,14 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
 
+    path('search/', views.search_movie, name='search_movie'),
+
+    path('vote/<int:movie_id>/', views.vote_movie, name='vote_movie'),
+
     # Test to check if API key was working
     #path('test-api/', views.test_api_key, name='test_api'),
 
     # Test to check if TMDB API integration was working
     #path('import/', views.import_movie),
-
-
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from .forms import MovieSearchForm
+
+from .utils import fetch_and_save_movie
+
+from .models import Movie
+
+from django.shortcuts import get_object_or_404, redirect
+
+from django.db.models import F
+
 from django.http import HttpResponse
+
 from django.conf import settings
 
 # Used to test TMDB API integration
@@ -20,4 +31,34 @@ def index(request):
 #     if movie:
 #         return HttpResponse(f"Imported movie: {movie.title}")
 #     return HttpResponse("Movie not found.")
+
+# Creates the view to search for movies
+def search_movie(request):
+    form = MovieSearchForm()
+    message = ""
+
+    if request.method == "POST":
+        form = MovieSearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            movie = fetch_and_save_movie(title)
+            if movie:
+                message = f"Movie '{movie.title}' imported successfully!"
+            else:
+                message = "Movie not found or could not be imported."
+
+    return render(request, "movies/search.html", {"form": form, "message": message})
+
+# Adds the movie view in index
+def index(request):
+    movies = Movie.objects.all().order_by('-release_date')
+    return render(request, 'movies/index.html', {'movies': movies})
+
+# Adds the voting view
+def vote_movie(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    movie.vote_count = F('vote_count') + 1
+    movie.save()
+    movie.refresh_from_db()  # Refresh to get updated vote_count
+    return redirect('movies:index')  # Go back to home page
 
