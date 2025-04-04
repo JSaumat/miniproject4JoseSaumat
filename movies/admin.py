b1d649a7fb2cb66ae3b8c352db4eeae1dc3516c1
JSaumat <j_saumat@mail.fhsu.edu>
2025-04-03 21:01:38 -0700

    Decided to completely remove the polls folders and all traces of polls from the project so there were no issues with either app bleeding into the other.

--- movies/admin.py
+++ movies/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Movie
 
+# Change admin site headers
+admin.site.site_header = "MovieVote Admin"
+admin.site.site_title = "MovieVote Admin Portal"
+admin.site.index_title = "Welcome to the MovieVote Management Area"
+
 # Register your models here.
 
 @admin.register(Movie)
